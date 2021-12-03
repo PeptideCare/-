5d412656dc2e907445a5a1802482f7929490b031
DESKTOP-86N6AHS\user <wldns997@naver.com>
2021-12-03 18:32:23 +0900

    21.12.03
    그래프

--- 서울지하철2호선_16947.java
+++ 서울지하철2호선_16947.java
@@ -4,6 +4,8 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class 서울지하철2호선_16947 {
@@ -14,13 +16,17 @@ public class 서울지하철2호선_16947 {
     static ArrayList<Integer>[] list;
     static ArrayList<Integer> sun = new ArrayList<>();
     static int start;
-    static int min;
+    static int[] ans;
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
         N = Integer.parseInt(br.readLine());
         list = new ArrayList[N+1];
+        ans = new int[N+1];
+
+        for (int i=1; i<=N; i++) {
+            ans[i] = 10000;
+        }
 
         for (int i=1; i<=N; i++) {
             list[i] = new ArrayList<>();
@@ -40,15 +46,14 @@ public class 서울지하철2호선_16947 {
             sun_dfs(i, 0);
         }
 
-        for (int i=1; i<=N; i++) {
-            min = Integer.MAX_VALUE;
-            for (int j=0; j<sun.size(); j++) {
-                visited = new boolean[N+1];
-                distance_dfs(i, sun.get(j), 0);
-            }
-            sb.append(min).append(" ");
+        for (int i=0; i<sun.size(); i++) {
+            visited = new boolean[N+1];
+            bfs(sun.get(i));
         }
-        System.out.println(sb);
+
+        for (int i=1; i<=N; i++) {
+            System.out.print(ans[i] + " ");
+        }
     }
 
     static void sun_dfs(int at, int depth) {
@@ -68,20 +73,27 @@ public class 서울지하철2호선_16947 {
 
     }
 
-    static void distance_dfs(int start, int des, int depth) {
+    static void bfs(int start) {
 
-        if (visited[start] == true) return;
-
-        if (start == des) {
-            min = Math.min(min, depth);
-            return;
-        }
-
+        Queue<Integer> q = new LinkedList<>();
         visited[start] = true;
+        q.offer(start);
+        ans[start] = 0;
 
-        for (int i=0; i<list[start].size(); i++) {
-            distance_dfs(list[start].get(i), des,depth+1);
+        while (!q.isEmpty()) {
+
+            int poll = q.poll();
+
+            for (int i=0; i<list[poll].size(); i++) {
+                int num = list[poll].get(i);
+
+                if (visited[num] == true) continue;
+
+                q.offer(num);
+                ans[num] = Math.min(ans[poll]+1, ans[num]);
+                visited[num] = true;
+
+            }
         }
     }
-
 }
